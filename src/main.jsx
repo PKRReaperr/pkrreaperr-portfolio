0cc6ff98388b7f00f5c83e3ef236fbc3bde2d0c3
Rayyan <rayyanfaisalsheikh@gmail.com>
2026-04-29 23:52:30 -0500

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from "./App"
 import "./index.css"
 
@@ -28,6 +29,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 )
